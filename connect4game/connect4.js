b25da05ab76cc8ddc21e062c96d424390876f487
Claudia El Khatib <claudiacv22@gmail.com>
2017-05-26 17:52:57 -0400

    js file modification(first part of code)

--- connect4game/connect4.js
+++ connect4game/connect4.js
@@ -7,3 +7,31 @@ var c = [0,0,0,0,0,0]
 var d = [0,0,0,0,0,0]
 var e = [0,0,0,0,0,0]
 var f = [0,0,0,0,0,0]
+
+function dropper(col) {
+  switch (col) {
+    case "a" :
+      colvalues = a.slice();
+      break;
+    case "b" :
+      colvalues = b.slice();
+      break;
+    case "c" :
+      colvalues = c.slice();
+      break;
+    case "d" :
+      colvalues = d.slice();
+      break;
+    case "e" :
+      colvalues = e.slice();
+      break;
+    case "f" :
+      colvalues = f.slice();
+      break;
+    case "g" :
+      colvalues = g.slice();
+      break;
+    default:
+      var colvalues = [];
+  }
+}
